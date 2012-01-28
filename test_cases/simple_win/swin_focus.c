0a4c5d242c041e552570c54bb7e423756bbc7ccf
Grissiom <chaos.proton@gmail.com>
2012-01-28 10:58:21 +0800

    fix a bug in test_case

--- test_cases/simple_win/swin_focus.c
+++ test_cases/simple_win/swin_focus.c
@@ -6,6 +6,8 @@
 #include <rtgui/widgets/label.h>
 #include <rtgui/widgets/button.h>
 
+struct rtgui_win *main_win;
+
 rt_bool_t picture_win_onpaint(struct rtgui_object* object, struct rtgui_event* event)
 {
 	struct rtgui_widget *widget = RTGUI_WIDGET(object);
@@ -42,7 +44,7 @@ rt_bool_t show_modal_info(struct rtgui_widget *object, struct rtgui_event *event
 	rtgui_label_t *label;
 	struct rtgui_win *win;
 	rtgui_rect_t rect = {0, 60, 150, 100};
-	win = rtgui_win_create(RT_NULL,
+	win = rtgui_win_create(main_win,
 						   "Info",
 						   &rect,
 						   RTGUI_WIN_STYLE_DEFAULT);
@@ -59,11 +61,15 @@ rt_bool_t show_modal_info(struct rtgui_widget *object, struct rtgui_event *event
 	rtgui_win_show(win, RT_TRUE);
 
 	rtgui_win_destroy(win);
+
+	return RT_TRUE;
 }
 
 rt_bool_t echo_btn_pressed(struct rtgui_widget *object, struct rtgui_event *event)
 {
 	rt_kprintf("object %p pressed\n", object);
+
+	return RT_TRUE;
 }
 
 rt_bool_t window_focus(void)
@@ -73,17 +79,17 @@ rt_bool_t window_focus(void)
 	rtgui_button_t* start_btn;
 	rtgui_button_t* stop_btn;
 	rtgui_toplevel_t *parent;
-    rtgui_win_t *win, *picture_win;
+    rtgui_win_t *picture_win;
 	rtgui_rect_t rect = {0, 20, 240, 320};
 
 	/* 创建一个窗口 */
-	win = rtgui_win_create(RT_NULL,
+	main_win = rtgui_win_create(RT_NULL,
 			"主窗口",
 			&rect,
-			RTGUI_WIN_STYLE_DEFAULT);
+			RTGUI_WIN_STYLE_DEFAULT | RTGUI_WIN_STYLE_DESTROY_ON_CLOSE);
 	/* 获得视图的位置信息 */
-	rtgui_widget_get_rect(RTGUI_WIDGET(win), &rect);
-	rtgui_widget_rect_to_device(RTGUI_WIDGET(win), &rect);
+	rtgui_widget_get_rect(RTGUI_WIDGET(main_win), &rect);
+	rtgui_widget_rect_to_device(RTGUI_WIDGET(main_win), &rect);
 	rect.x1 += 10;
 	rect.x2 -= 5;
 	rect.y2 = rect.y1 + 20;
@@ -93,12 +99,12 @@ rt_bool_t window_focus(void)
 	/* 设置标签位置信息 */
 	rtgui_widget_set_rect(RTGUI_WIDGET(label), &rect);
 	/* 添加标签到视图中 */
-	rtgui_container_add_child(RTGUI_CONTAINER(win),
+	rtgui_container_add_child(RTGUI_CONTAINER(main_win),
 			RTGUI_WIDGET(label));
 
 	/* 获得视图的位置信息 */
-	rtgui_widget_get_rect(RTGUI_WIDGET(win), &rect);
-	rtgui_widget_rect_to_device(RTGUI_WIDGET(win), &rect);
+	rtgui_widget_get_rect(RTGUI_WIDGET(main_win), &rect);
+	rtgui_widget_rect_to_device(RTGUI_WIDGET(main_win), &rect);
 	rect.x1 += 10;
 	rect.y2 -= 10;
 	rect.y1 = rect.y2 - 25;
@@ -112,12 +118,12 @@ rt_bool_t window_focus(void)
 	rtgui_button_set_onbutton(start_btn, show_modal_info);
 
 	/* 添加按钮到视图中 */
-	rtgui_container_add_child(RTGUI_CONTAINER(win),
+	rtgui_container_add_child(RTGUI_CONTAINER(main_win),
 			RTGUI_WIDGET(start_btn));
 
 	/* 添加停止按钮*/
-	rtgui_widget_get_rect(RTGUI_WIDGET(win), &rect);
-	rtgui_widget_rect_to_device(RTGUI_WIDGET(win), &rect);
+	rtgui_widget_get_rect(RTGUI_WIDGET(main_win), &rect);
+	rtgui_widget_rect_to_device(RTGUI_WIDGET(main_win), &rect);
 	rect.x2 -= 10;
 	rect.y2 -= 10;
 	rect.x1 = rect.x2 - 50;
@@ -131,13 +137,13 @@ rt_bool_t window_focus(void)
 	rtgui_button_set_onbutton(stop_btn, echo_btn_pressed);
 
 	/* 添加按钮到视图中 */
-	rtgui_container_add_child(RTGUI_CONTAINER(win),
+	rtgui_container_add_child(RTGUI_CONTAINER(main_win),
 			RTGUI_WIDGET(stop_btn));
 
 	parent = RTGUI_TOPLEVEL(rtgui_widget_get_toplevel(RTGUI_WIDGET(start_btn)));
 	/*创建一个绘图Windows控件*/
-	rtgui_widget_get_rect(RTGUI_WIDGET(win), &rect);
-	rtgui_widget_rect_to_device(RTGUI_WIDGET(win), &rect);
+	rtgui_widget_get_rect(RTGUI_WIDGET(main_win), &rect);
+	rtgui_widget_rect_to_device(RTGUI_WIDGET(main_win), &rect);
 	rect.x1 += 10;
 	rect.y1 += 20;
 	rect.x2 = rect.x1 + 200;
@@ -147,17 +153,16 @@ rt_bool_t window_focus(void)
             RTGUI_WIN_STYLE_NO_TITLE|RTGUI_WIN_STYLE_NO_BORDER|RTGUI_WIN_STYLE_NO_FOCUS);
     //创建窗口，没有标题栏，没有最小化窗口，也不能获取焦点
 	/* 添加windows的事件*/
-	rtgui_container_add_child(RTGUI_CONTAINER(win),
+	rtgui_container_add_child(RTGUI_CONTAINER(main_win),
 			RTGUI_WIDGET(picture_win));
 	rtgui_object_set_event_handler(RTGUI_OBJECT(picture_win),
 			picture_win_onpaint);
 
 	/* 非模态显示窗口 */
-	rtgui_widget_focus(RTGUI_WIDGET(win));//设定主窗体获取焦点
-	rtgui_win_show(win, RT_FALSE);
+	rtgui_widget_focus(RTGUI_WIDGET(main_win));//设定主窗体获取焦点
+	rtgui_win_show(main_win, RT_FALSE);
 	rtgui_win_show(picture_win,RT_FALSE);
 
-	/* 执行工作台事件循环 */
 	return RT_TRUE;
 }
 
