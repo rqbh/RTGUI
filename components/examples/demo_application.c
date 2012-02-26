98432b83ecb15fa9413a4922817c9de9dc2959b3
Grissiom <chaos.proton@gmail.com>
2012-02-26 14:04:05 +0800

    port more demo

--- components/examples/demo_application.c
+++ components/examples/demo_application.c
@@ -85,9 +85,9 @@ static void application_entry(void* parameter)
 	demo_view_scrollbar();
 	demo_view_radiobox();
 	demo_view_textbox();
-#if 0
 	demo_view_listbox();
 	demo_view_menu();
+#if 0
 	demo_view_listctrl();
 	demo_view_combobox();
 	demo_view_slider();
