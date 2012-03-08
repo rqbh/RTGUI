ebe44c557435d455ebec1a3683a95480d2aba9c5
Grissiom <chaos.proton@gmail.com>
2012-03-08 16:18:11 +0800

    remove some old API and misc cleanup

--- components/examples/demo_listview.c
+++ components/examples/demo_listview.c
@@ -1,7 +1,7 @@
 /*
  * 程序清单：列表视图演示
  *
- * 这个例子会先创建出一个演示用的view，当点击上面的按钮时会按照模式显示的形式显示
+ * 这个例子会先创建出一个演示用的container，当点击上面的按钮时会按照模式显示的形式显示
  * 新的列表视图
  */
 #include "demo_view.h"
@@ -15,11 +15,7 @@ static rtgui_list_view_t* _view = RT_NULL;
 // static rtgui_image_t* return_image = RT_NULL;
 
 /* 列表项的动作函数 */
-#if RT_VERSION == 4
 static void listitem_action(rtgui_widget_t *widget, void* parameter)
-#else
-static void listitem_action(void* parameter)
-#endif
 {
 	char label_text[32];
 	rtgui_win_t *win;
@@ -43,18 +39,14 @@ static void listitem_action(void* parameter)
 	label = rtgui_label_create(label_text);
 
 	rtgui_widget_set_rect(RTGUI_WIDGET(label), &rect);
-	rtgui_container_add_child(RTGUI_CONTAINER(win), RTGUI_WIDGET(label));
+	rtgui_container_add_child(win, RTGUI_WIDGET(label));
 
 	/* 非模态显示窗口 */
 	rtgui_win_show(win, RT_FALSE);
 }
 
 /* 返回功能的动作函数 */
-#if RT_VERSION == 4
 static void return_action(rtgui_widget_t* widget, void* parameter)
-#else
-static void return_action(void* parameter)
-#endif
 {
 	rtgui_container_end_modal(RTGUI_CONTAINER(_view), RTGUI_MODAL_OK);
 }
@@ -92,24 +84,24 @@ static void open_btn_onbutton(rtgui_widget_t* widget, struct rtgui_event* event)
 }
 
 /* 创建用于演示列表视图的视图 */
-rtgui_container_t* demo_listview_view(struct rtgui_application *app)
+rtgui_container_t* demo_listview_view(void)
 {
 	rtgui_rect_t rect;
-	rtgui_container_t *view;
+	rtgui_container_t *container;
 	rtgui_button_t* open_btn;
 
-	view = demo_view(app, "列表视图演示");
+	container = demo_view("列表视图演示");
 
 	/* 添加动作按钮 */
-	demo_view_get_rect(view, &rect);
+	demo_view_get_rect(container, &rect);
 	rect.x1 += 5;
 	rect.x2 = rect.x1 + 80;
 	rect.y1 += 30;
 	rect.y2 = rect.y1 + 20;
 	open_btn = rtgui_button_create("打开列表");
-	rtgui_container_add_child(RTGUI_CONTAINER(view), RTGUI_WIDGET(open_btn));
+	rtgui_container_add_child(container, RTGUI_WIDGET(open_btn));
 	rtgui_widget_set_rect(RTGUI_WIDGET(open_btn), &rect);
 	rtgui_button_set_onbutton(open_btn, open_btn_onbutton);
 
-	return view;
+	return container;
 }
